a8d1e3a0dd3e3f5247618ea2e183ecd7065a8fdc
shawncolbert <59206422+shawncolbert@users.noreply.github.com>
2026-06-10 10:45:21 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'ship2shore-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/success.html'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
